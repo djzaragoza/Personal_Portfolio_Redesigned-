9c0e9dfcf7114a40960e44720b42f890aaad496e
unknown <dj.zaragoza@icloud.com>
2020-02-12 21:33:48 -0800

    lighting round

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
-; <form className = 'contact-form'
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+
+const app = express();
+
+const port = 4444;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors());
+
+app.listen(port, () => {
+    console.log('We are live on port 4444');
+});
+
+app.get('/', (req, res) => {
+    res.send('Welcome to my API');
+})
+
+app.post('/api/v1', (req, res) => {
+    var data = req.body;
+
+    var smtpTransport = nodemailer.createTransport({
+        service: 'Gmail',
+        port: 465,
+        auth: {
+            user: 'USERNAME',
+            pass: 'PASSWORD'
+        }
+    });
+};
+
+var mailOptions = {
+    from: data.email,
+    to: 'Enter_Your_Email',
+    subject: 'Enter Your Subject',
+    html: `<p>${data.name}</p>
+           <p>${data.email}</p>
+           <p>${data.message}</p>`
+};
+
+smtpTransport.sendMail(mailOptions,
+    (error, response) => {
+        if(error) {
+            res.send(error)
+        } else {
+            res.send('Success')
+        }
+        smtpTransport.close();
+    });
+
+})
+
+app.post('/api/v1', (req, res) => {
+    var data = 
+})
+
+<form className = 'contact-form'
 onSubmit = {
         e => this.formSubmit(e)
     } >
@@ -104,4 +164,3 @@ resetForm = () => {
         buttonText: 'Message Sent'
     })
 }
-
